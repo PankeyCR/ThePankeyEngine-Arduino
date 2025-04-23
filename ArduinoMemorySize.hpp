cc943d91e192697eede4e604244cfbbf530c69d5
PankeyCR <internet_higgs@hotmail.es>
2025-04-23 16:48:36 -0600

    adding the ps ram for the esp32

---
+++ ArduinoMemorySize.hpp
@@ -0,0 +1,17 @@
+
+#ifndef ArduinoMemorySize_hpp
+	#define ArduinoMemorySize_hpp
+
+	namespace pankey{
+
+		namespace Base{
+
+			#ifndef MemorySize(TYPE)
+
+				using memory_size = size_t;
+
+			#endif
+		}
+	}
+
+#endif
